3a4c89d1f1179d2a187456cb6779ebbb8c9150f6
Soujatya Das <soujatya548@gmail.com>
2025-01-27 01:55:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.embeddings import HuggingFaceEmbeddings
 
 template = """
-You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. As per the question asked, please mention the related information in full and use point-wise format wherever necessary.
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. As per the question asked, please mention the accurate related information in full and use point-wise format wherever necessary.
 Question: {question} 
 Context: {context} 
 Answer:
